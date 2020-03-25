bca90a9450451d56c995de8f82d78a06e2f7af5d
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-25 09:58:16 -0400

    added in CSS rules per doc, outcome unexpected

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,14 +1,23 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class MemoryCard extends Component {
-    render() {
-        return (
-      <div class="MemoryCard">
-                   <img class="dcimg"
-                   src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png
-                   " alt=""/>
-      </div> )
-    }
+  render() {
+    return (
+      <div className='MemoryCard'>
+        <div className='MemoryCardInner'>
+          <div className='MemoryCardBack'>
+            <img
+              class='dcimg'
+              src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png
+                   '
+              alt=''
+            />
+          </div>
+          <div className='MemoryCardFront'>∆</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MemoryCard;
