084313e0ea6ab258125d304a0e7dd99566fd54c4
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-26 16:02:17 -0400

    added in generate deck and shuffle functions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,40 @@ import './App.css';
 import './MemoryCard.css'
 import MemoryCard from "./components/MemoryCard";
 
+const generateDeck = () => {
+  const symbols = [`∆`,` ß`, `£`, `§`,`•`, `$`, `+`, `ø`]
+  let deck = []
+  
+  for (let i = 0; i < 16; i++){
+    deck.push({
+      isFlipped: false, 
+      symbol: symbols[i % 8]
+    });
+}
+return shuffle(deck)
+}
+
+const shuffle = () => {
+  let j = '';
+  let temp = '';
+  for (let i = deck.length -1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
+  return deck;
+}
+
+
 function App() {
+  constructor() {
+    super();
+    this.state = {
+      deck: [],
+      pickedCards: []
+    };
+  }
   return (
     <div className="App">
       <header className="App-header">
